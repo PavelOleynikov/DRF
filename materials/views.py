1764f6c1b8d02789d6b5dd13da8df6fff281c90c
Pavel Oleynikov <Pavlushka19805@gmail.com>
2026-01-29 22:24:26 +0400

    create CRUD for models Course, Lesson using Viewsets and Generic

--- materials/views.py
+++ materials/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework import viewsets, generics
 
-# Create your views here.
+from materials.models import Course, Lesson
+from materials.serializers import CourseSerializer, LessonSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    """Класс для работы с курсами"""
+
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class LessonCreateAPIView(generics.CreateAPIView):
+    """Класс для создания урока"""
+
+    serializer_class = LessonSerializer
+
+
+class LessonListAPIView(generics.ListAPIView):
+    """Класс для получения списка уроков"""
+
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonRetrieveAPIView(generics.RetrieveAPIView):
+    """ " Класс для получения конкретного урока"""
+
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonUpdateAPIView(generics.UpdateAPIView):
+    """ " Класс для редактирования урока"""
+
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonDestroyAPIView(generics.DestroyAPIView):
+    """Класс для удаления урока"""
+
+    queryset = Lesson.objects.all()
