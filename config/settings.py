2d7f2f46d312c3ba1b6e43d0ecddc084ea184383
Pavel Oleynikov <Pavlushka19805@gmail.com>
2026-01-29 15:13:07 +0400

    create app users

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+
 from dotenv import load_dotenv
 
 load_dotenv(override=True)  # override=True - перезаписывает переменные окружения
@@ -19,6 +20,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -85,8 +88,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+# STATIC_URL = "static/"
+# STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
