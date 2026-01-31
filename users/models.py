2f02324192ffe3c0b0b8ebd405381667bb001b96
Pavel Oleynikov <Pavlushka19805@gmail.com>
2026-01-31 14:17:31 +0400

    add field lessons_count in CourseSerializer, create Payments model

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from materials.models import Course, Lesson
+
 
 class User(AbstractUser):
 
@@ -16,3 +18,32 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Payments(models.Model):
+    """Класс для работы с платежами"""
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="payments", verbose_name="пользователь")
+    payment_date = models.DateTimeField(auto_now_add=True, verbose_name="дата платежа")
+    paid_course = models.ForeignKey(
+        Course, on_delete=models.CASCADE, related_name="paid_course", verbose_name="оплаченный курс"
+    )
+    paid_lesson = models.ForeignKey(
+        Lesson, on_delete=models.CASCADE, related_name="paid_lesson", verbose_name="оплаченный урок"
+    )
+    amount = models.DecimalField(verbose_name="сумма оплаты", max_digits=10, decimal_places=2)
+    method_payment = models.CharField(
+        max_length=10,
+        choices=[("cash", "Наличные"), ("transfer", "Перевод на счет")],
+        verbose_name="Способ оплаты",
+        help_text="Выберите способ оплаты",
+    )
+
+    class Meta:
+        verbose_name = "Платеж"
+        verbose_name_plural = "Платежи"
+        ordering = ["-payment_date"]
+        unique_together = ["user", "paid_course", "paid_lesson"]
+
+    def __str__(self):
+        return f"{self.user} - {self.paid_course} - {self.paid_lesson}"
