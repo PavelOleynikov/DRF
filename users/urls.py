282ec02218747775d714b1fb5fdedcf36912c6d3
Pavel Oleynikov <Pavlushka19805@gmail.com>
2026-02-10 20:43:55 +0400

    add settings permissions.IsAuthenticated and AllowAny in UserCreate, Userlogin

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.permissions import AllowAny
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -15,6 +16,6 @@ router.register(r"", UserViewSet, basename="users")
 
 urlpatterns = [
     path("register/", UserCreateAPIView.as_view(), name="register"),
-    path("login/", TokenObtainPairView.as_view(), name="login"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("login/", TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(permission_classes=(AllowAny,)), name="token_refresh"),
 ] + router.urls
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
 
 from users.models import User
 from users.serializers import UserCreateSerializer, UserDetailViewSerializer, UserViewSerializer
@@ -10,6 +11,7 @@ class UserCreateAPIView(CreateAPIView):
 
     serializer_class = UserCreateSerializer
     queryset = User.objects.all()
+    permission_classes = (AllowAny,)
 
     def perform_create(self, serializer):
         user = serializer.save(is_active=True)
