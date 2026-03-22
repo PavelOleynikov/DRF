e154b1b9d73f438fd959fa7aafe5a9594bedb561
Pavel Oleynikov <Pavlushka19805@gmail.com>
2026-03-22 12:48:29 +0400

    changed settings, docker-compose

--- config/settings.py
+++ config/settings.py
@@ -87,7 +87,7 @@ else:
         "default": {
             "ENGINE": "django.db.backends.postgresql_psycopg2",
             "NAME": os.getenv("NAME"),
-            "USER": os.getenv("USER"),
+            "USER": os.getenv("DB_USER"),
             "PASSWORD": os.getenv("PASSWORD"),
             "HOST": os.getenv("HOST"),
             "PORT": os.getenv("PORT"),
